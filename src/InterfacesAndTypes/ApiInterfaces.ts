f12c12cc3645ae7dfaaedf4bba429144bcebe079
clint911 <clingyking007@gmail.com>
2023-07-10 01:07:28 +0300

    commit before breaking everything

---
+++ src/InterfacesAndTypes/ApiInterfaces.ts
@@ -0,0 +1,58 @@
+export interface cartItems {
+  productIds: number[];
+}
+export enum userRole {
+  admin,
+  buyer,
+  seller
+}
+
+export interface userData extends cartItems {
+  userId: number;
+  email: string;
+  userName: string;
+  password: string;
+  address: string;
+  city: string;
+  country: string;
+  role: userRole
+}
+export enum ProductRating {
+  one,
+  two,
+  three,
+  four,
+  five
+}
+export interface Reviews {
+  reviewId: number;
+  productId: number;
+  userId: number;
+  productRating: ProductRating;
+  textContent: string;
+  dateOfReview: string;//TODO: refactor to use Date
+}
+
+export interface Products {
+  productId: number;
+  price: number;
+  productName: string;
+  description: string;
+  imageUrl: string;
+}
+export enum OrderStatus {
+  pending,
+  approved,
+  shipped,
+  completed,
+  failed
+}
+export interface Orders {
+  orderId: number;
+  noOfProducts: number;
+  totalPrice: number;
+  orderStatus: OrderStatus;
+  date: string;//TODO: refactor implementation to use Date
+  productsOrdered: string;//TODO: refactor implementation to use array
+}
+
